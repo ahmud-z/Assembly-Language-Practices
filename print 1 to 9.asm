b00ce7489c3473fee46e53c1fdd28bc52a60b4c0
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2023-10-19 21:54:32 +0600

    print 1 to 9 in descending order

---
+++ print 1 to 9.asm
@@ -0,0 +1,34 @@
+.model small
+
+.stack 100h
+.code 
+ 
+main proc
+ 
+ MOV AX,1
+ MOV CX,9
+ 
+ 
+ my_loop:
+      
+      
+     MOV DL,CL
+     ADD DL,48    
+     MOV AH,2        ;print single CL value
+     INT 21H 
+      
+     MOV AH,2
+     MOV DL,20h      ;print single space
+     INT 21H      
+    
+ loop my_loop            
+ 
+         
+         
+         
+         
+    MOV AH,4CH
+    INT 21H 
+         
+    main endp
+    end main
